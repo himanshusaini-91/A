ffcc79b0f72dc7f88bd8284dd58ded59ffbc2e2e
himanshusaini-91 <himanshusaini0055@gmail.com>
2016-04-03 15:03:22 +0530

    first coomit

---
+++ a/pro/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the proApp
+ */
+angular.module('proApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ a/pro/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the proApp
+ */
+angular.module('proApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
