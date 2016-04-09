2f320a76732ed586a3be62a4d7fbc3a1860a5130
himanshusaini-91 <himanshusaini0055@gmail.com>
2016-04-09 12:11:10 +0530

    Validation
    check this out

---
+++ validation/controller.js
@@ -0,0 +1,21 @@
+/**
+ * Created by tanvirsaini32 on 3/04/2016.
+ */
+var validationApp = angular.module('validationApp', ['ngMessages']);
+
+// create angular controller
+validationApp.controller('mainController', function($scope) {
+
+    // function to submit the form after all validation has occurred
+    $scope.submitForm = function(isValid) {
+
+        // check to make sure the form is completely valid
+        if (isValid) {
+            alert('our form is amazing');
+        }
+
+    };
+
+});
+
+
